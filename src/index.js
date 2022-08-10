8f6003080df7eba471649da418b89d0277771e47
kinkade21 <victorpushanov@mail.ru>
2022-08-10 15:31:12 +0300

    scroll2

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import {timer}  from "./modules/timer";
 import {sertificates}  from "./modules/sertificates";
 import {scroll}  from "./modules/scroll";
 import modal from "./modules/modal";
-import slider from "./modules/slider";
+
 
 
 
@@ -18,4 +18,3 @@ timer("01 october 2022");
 sertificates();
 scroll();
 modal();
-slider();
--- src/modules/slider.js
+++
@@ -1,8 +0,0 @@
-const slider = () => {
-
-
-
-}
-
-
-export default slider
